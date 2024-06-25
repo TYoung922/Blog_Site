0b62a1a02c244f9f5c2426596d9b30565b97b0a8
TercanoNuruva <thyoung89@gmail.com>
2024-06-25 09:39:54 -0600

    admin files added

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
 const connectDB = require("./server/config/db");
 connectDB();
 
@@ -17,6 +22,25 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+
+//admin
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+// Duplicate your route file below to also have a admin instead of main.
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 // const insertPostData = require("./server/routes/dummydate");
 // insertPostData();
